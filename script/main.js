ba2a5229a220f7dff3f9a8d1de4953766e2c6a27
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-03-22 00:19:49 +0600

    just added page

---
+++ script/main.js
@@ -0,0 +1,13 @@
+document.getElementById("forBlog").addEventListener("click",function(){
+    window.location.href="blog.html";
+})
+
+
+// complete btn click for 1.alert (board update succesfully) then task assigned decrease and increase 23 to 24 then add child p tag in sidebar div with at time date.
+// complete btn disable**
+
+
+// live time show challenges
+// clear history btn for clear all history
+// color change random used with help of google
+// last complete provide a alert 
